c60c4d1f65db9540f2149c3059e62972f9d5d830
marco.baltazar <marco.baltazar@jalasoft.com>
2020-08-26 17:10:09 -0400

    add babel configuration to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { buildSchema } from 'graphql';
+import 'dotenv/config';
 
 var schema = buildSchema(`
   type Query {
@@ -8,12 +9,12 @@ var schema = buildSchema(`
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+const root = { hello: () => 'Hello world!' };
 
-var app = express();
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+app.listen(process.env.BACKEND_PORT, () => console.log(`Now browse to localhost:${process.env.BACKEND_PORT}/graphql`));
