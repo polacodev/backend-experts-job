e198ce91f3420dba1e9bf5c1990b7248623a9fbd
Marco Baltazar <Marco.Baltazar@highjump.com>
2021-01-07 22:18:26 -0400

    add subscription to the user after add

--- src/components/user/user.resolver.js
+++ src/components/user/user.resolver.js
@@ -1,6 +1,7 @@
 import {
   GraphQLNonNull, GraphQLID, GraphQLList,
 } from 'graphql';
+
 import {
   getAllUsersAPI, getUserByIdAPI, addUserAPI,
   updateUserAPI, deleteUserAPI, getUsersFilterAPI,
@@ -8,13 +9,23 @@ import {
 import UserInput from '../../types/user.type';
 import SearchUserType from '../../types/search.type';
 import userType from './user.schema';
+import pubsub from '../../config/pub-subscription';
+
+export const userAdded = {
+  type: userType,
+  subscribe: () => pubsub.asyncIterator('USER_ADDED'),
+};
 
 export const addUser = {
   type: userType,
   args: {
     user: { type: new GraphQLNonNull(UserInput) },
   },
-  resolve: (obj, args) => addUserAPI(args),
+  resolve: async (obj, args) => {
+    const data = await addUserAPI(args);
+    pubsub.publish('USER_ADDED', { userAdded: data });
+    return data;
+  },
 };
 
 export const updateUser = {
--- src/subscriptions/index.js
+++ src/subscriptions/index.js
@@ -1,10 +1,12 @@
 import { GraphQLObjectType } from 'graphql';
 import { addNotification } from '../components/notification/notification.resolver';
+import { userAdded } from '../components/user/user.resolver';
 
 const subscriptionType = new GraphQLObjectType({
   name: 'Subscription',
   fields: {
     addNotification,
+    userAdded,
   },
 });
 
